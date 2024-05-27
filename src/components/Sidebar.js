7419e179ff14df874a1f52c54ae39b5937cccd6f
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-05-27 10:45:23 +0530

    Siderbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  // Early retun Pattern
+  if (!isMenuOpen) return null;
+
   return (
-    <div className="p-5 shadow-lg w-52">
+    <div className="p-5 shadow-lg w-48 ">
       <ul>
         <li>Home</li>
         <li>Shorts</li>
