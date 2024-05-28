8fdcb6da21af302d14de6f6cf602a35e6767f6da
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-05-28 08:23:09 +0530

    Video Card Component

---
+++ src/components/VideoCard.js
@@ -0,0 +1,20 @@
+const VideoCard = ({ info }) => {
+  console.log("info ", info);
+
+  const { snippet, statistics } = info;
+  const { channelTitle, title, thumbnails } = snippet;
+  console.log("CT", channelTitle);
+  return (
+    <div className=" p-2 m-2 w-80  cursor-pointer">
+      <img className="rounded-lg" src={thumbnails.medium.url} alt="video" />
+      <ul>
+        <li>{}</li>
+        <li className="font-bold py-2">{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics.viewCount} views</li>
+      </ul>
+    </div>
+  );
+};
+
+export default VideoCard;
