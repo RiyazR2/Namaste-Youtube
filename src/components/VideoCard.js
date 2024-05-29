fb071c3542eb6142385e7da77c4cd94cf6679134
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-05-29 09:28:37 +0530

    Updated

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -8,7 +8,6 @@ const VideoCard = ({ info }) => {
     <div className=" p-2 m-2 w-80  cursor-pointer">
       <img className="rounded-lg" src={thumbnails.medium.url} alt="video" />
       <ul>
-        <li>{}</li>
         <li className="font-bold py-2">{title}</li>
         <li>{channelTitle}</li>
         <li>{statistics.viewCount} views</li>
