f11b4e5508366ab5f43ab4ec6decb4fed1903b52
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-05-30 10:08:31 +0530

    Updated Store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appSlice from "./appSlice";
 
-const store = configureStore({});
+const store = configureStore({
+  reducer: { app: appSlice },
+});
 
 export default store;
