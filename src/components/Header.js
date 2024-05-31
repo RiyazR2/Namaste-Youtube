c3a2769b4a4c7d91dbc621f7c945457b550394bf
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-05-31 10:11:37 +0530

    Updates on Suggestion API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import Menu_Logo from "../utils/Images/Menu_Logo.png";
 import Youtube_Logo from "../utils/Images/Youtube_Logo.png";
+import { url } from "../utils/constants";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestion(), 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestion = async () => {
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    const data = await fetch(url + searchQuery);
+    const json = await data.json();
+    console.log(json);
+  };
+
   const dispatch = useDispatch();
 
   const toggleMenuHandler = () => {
@@ -27,14 +44,22 @@ const Header = () => {
       <div className="col-span-10 px-10">
         <div>
           <input
-            className="px-5 w-1/2 border border-gray-400 p-2 ml-32 rounded-l-full"
+            className="px-5 w-1/2 border border-gray-400 p-2  rounded-l-full"
             type="text"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
           />
           <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
             🔍
           </button>
         </div>
-        <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100"></div>
+        <div className="   bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+          <ul>
+            <li className="py-2 px-3 shadow-sm hover:bg-gray-100">🔍 iPhone</li>
+          </ul>
+        </div>
       </div>
       <div className="col-span-1">
         <img
