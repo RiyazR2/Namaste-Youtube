f460bca0d618185461b5a38568c83119f855d9a6
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-09 10:58:20 +0530

    Updated Search API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,37 +1,41 @@
 const GOOGLE_API_KEY = "AIzaSyC4K2K2vJF_rz7IgmeKkeZ4VYOwCy3irA4";
+// 2nd key = "AIzaSyB-v1j1S61-kah-edlFSSvYA6sQEaL0IHU"
+
+export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
 
-// export const YOUTUBE_SEARCH_API =
+// export const YOUTUBE_SEARCH_API1 =
 //   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const YOUTUBE_SEARCH_API =
-  "https://corsproxy.io/?" +
-  encodeURIComponent(
-    "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
-  );
+// export const YOUTUBE_SEARCH_API2 =
+//   "https://corsproxy.io/?" +
+//   encodeURIComponent(
+//     "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+//   );
 
-// export const YOUTUBE_SEARCH_API2 = "https://http-cors-proxy.p.rapidapi.com/";
-// const options = {
-//   method: "POST",
-//   headers: {
-//     "content-type": "application/json",
-//     Origin: "www.example.com",
-//     "X-Requested-With": "www.example.com",
-//     "X-RapidAPI-Key": "815b0bc3b1msh35b258dcc7ede8ep10041fjsn70ba9caaa79f",
-//     "X-RapidAPI-Host": "http-cors-proxy.p.rapidapi.com",
-//   },
-//   body: {
-//     url: "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=Query",
-//   },
-// };
+////////////////////////////////////////////////////////////////////////////////////
+export const YOUTUBE_SEARCH_API3 = "https://http-cors-proxy.p.rapidapi.com/";
+const options = {
+  method: "POST",
+  headers: {
+    "content-type": "application/json",
+    Origin: "www.example.com",
+    "X-Requested-With": "www.example.com",
+    "X-RapidAPI-Key": "815b0bc3b1msh35b258dcc7ede8ep10041fjsn70ba9caaa79f",
+    "X-RapidAPI-Host": "http-cors-proxy.p.rapidapi.com",
+  },
+  body: {
+    url: "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=Query",
+  },
+};
 
-// try {
-//   const response = await fetch(YOUTUBE_SEARCH_API2, options);
-//   const result = await response.text();
-//   console.log(result);
-// } catch (error) {
-//   console.error(error);
-// }
+try {
+  const response = await fetch(YOUTUBE_SEARCH_API3, options);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
