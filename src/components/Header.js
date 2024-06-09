5ba33d1aede51e16555925fab390c46f1c812a41
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-06-09 11:01:45 +0530

    updated on Suggestions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import Menu_Logo from "../utils/Images/Menu_Logo.png";
 import Youtube_Logo from "../utils/Images/Youtube_Logo.png";
-import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { YOUTUBE_SEARCH_API1 } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
 
+  const searchCache = useSelector((store) => store.search);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestion(), 200);
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestion();
+      }
+    }, 200);
     return () => {
       clearTimeout(timer);
     };
   }, [searchQuery]);
 
   const getSearchSuggestion = async () => {
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    console.log("API Calls -", searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API1 + searchQuery);
     const json = await data.json();
-    console.log(json[1]);
+    // console.log(json[1]);
     setSuggestions(json[1]);
-  };
 
-  const dispatch = useDispatch();
+    //Update Cache
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -50,18 +65,16 @@ const Header = () => {
             className="px-5 w-1/2 border border-gray-400 p-2  rounded-l-full"
             type="text"
             value={searchQuery}
-            onChange={(e) => {
-              setSearchQuery(e.target.value);
-            }}
-            onFocus={() => setShowSuggestions(true)}
-            onBlur={() => setShowSuggestions(false)}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)} // Focus in
+            onBlur={() => setShowSuggestions(false)} // Focus out
           />
           <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
             🔍
           </button>
         </div>
         {showSuggestions && (
-          <div className="   bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+          <div className="  sticky bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
             <ul>
               {suggestions.map((s) => (
                 <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
@@ -84,110 +97,3 @@ const Header = () => {
 };
 
 export default Header;
-
-//////////////////////////////////////////////////////
-
-// import React, { useEffect, useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { toggleMenu } from "../utils/appSlice";
-// import Menu_Logo from "../utils/Images/Menu_Logo.png";
-// import Youtube_Logo from "../utils/Images/Youtube_Logo.png";
-// import { url } from "../utils/constants";
-
-// const Header = () => {
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [suggestions, setSuggestions] = useState([]);
-
-//   useEffect(() => {
-//     if (searchQuery) {
-//       const timer = setTimeout(() => getSearchSuggestion(), 200);
-//       return () => {
-//         clearTimeout(timer);
-//       };
-//     } else {
-//       setSuggestions([]);
-//     }
-//   }, [searchQuery]);
-
-//   const getSearchSuggestion = async () => {
-//     const searchUrl = `${url}${searchQuery}`;
-//     console.log("Fetching from URL:", searchUrl); // Log the full URL
-
-//     try {
-//       const response = await fetch(searchUrl);
-//       if (!response.ok) {
-//         console.error(
-//           "Network response was not ok:",
-//           response.status,
-//           response.statusText
-//         );
-//         throw new Error(
-//           `Network response was not ok: ${response.status} ${response.statusText}`
-//         );
-//       }
-//       const json = await response.json();
-//       setSuggestions(Array.isArray(json[1]) ? json[1] : []); // Ensure json[1] is an array
-//     } catch (error) {
-//       console.error("Failed to fetch search suggestions:", error);
-//       setSuggestions([]); // Set to an empty array on error
-//     }
-//   };
-
-//   const dispatch = useDispatch();
-
-//   const toggleMenuHandler = () => {
-//     dispatch(toggleMenu());
-//   };
-
-//   return (
-//     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
-//       <div className="flex col-span-1">
-//         <img
-//           onClick={() => toggleMenuHandler()}
-//           className="h-8 cursor-pointer"
-//           alt="menu"
-//           src={Menu_Logo}
-//         />
-//         <a href="/">
-//           <img className="h-8 mx-2" alt="youtube-logo" src={Youtube_Logo} />
-//         </a>
-//       </div>
-//       <div className="col-span-10 px-10">
-//         <div>
-//           <input
-//             className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
-//             type="text"
-//             value={searchQuery}
-//             onChange={(e) => setSearchQuery(e.target.value)}
-//           />
-//           <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-//             🔍
-//           </button>
-//         </div>
-//         {suggestions.length > 0 && (
-//           <div className="bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
-//             <ul>
-//               {suggestions.map((s, index) => (
-//                 <li
-//                   key={index}
-//                   className="py-2 px-3 shadow-sm hover:bg-gray-100"
-//                 >
-//                   🔍 {s}
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         )}
-//       </div>
-//       <div className="col-span-1">
-//         <img
-//           className="h-8"
-//           alt="user"
-//           src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
