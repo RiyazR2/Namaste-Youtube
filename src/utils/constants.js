75957800e154d93b3f82a4b5687bbec4a5a12345
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-15 18:34:09 +0530

    Updated API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 const GOOGLE_API_KEY = "AIzaSyB-v1j1S61-kah-edlFSSvYA6sQEaL0IHU";
 // 2nd key = ""
 //https://console.cloud.google.com/apis/credentials?project=ace-fiber-309604 ==> go and update key from this
-export const LIVE_CHAT_COUNT = 25;
+export const LIVE_CHAT_COUNT = 20;
 
 // Most Popular Videos API
 export const YOUTUBE_VIDEOS_API =
@@ -12,3 +12,12 @@ export const YOUTUBE_VIDEOS_API =
 export const YOUTUBE_SEARCH_API =
   "https://cors-handlers.vercel.app/api/?url=http%3A%2F%2Fsuggestqueries.google.com%2Fcomplete%2Fsearch%3Fclient%3Dfirefox%26ds%3Dyt%26q%3D";
 // "https://thingproxy.freeboard.io/fetch/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const YOUTUBE_HOME_ICON =
+  "https://t4.ftcdn.net/jpg/07/13/03/55/360_F_713035523_QJAAIPZnNnBipmwYhz0WruLe6KVQfxaH.jpg";
+
+export const YOUTUBE_SHORTS_ICON =
+  "https://seeklogo.com/images/Y/youtube-shorts-logo-E2B507EF18-seeklogo.com.png";
+
+export const YOUTUBE_SUBSCRIPTION_ICON =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUF2WvlfBFamFtc_WwmyBsCHgP6WdvqAGqKw&s";
