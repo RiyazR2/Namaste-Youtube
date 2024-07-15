dbce99f5ffe6f2851be47ada6bb217b166dc9377
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-15 18:36:32 +0530

    Updated style

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,11 +2,11 @@ const ChatMessage = ({ name, message }) => {
   return (
     <div className="flex items-center shadow-sm p-2">
       <img
-        className="h-8"
+        className="h-6"
         alt="user"
         src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
       />
-      <span className="font-bold px-2">{name}</span>
+      <span className="font-semibold px-2 text-sm">{name}:</span>
       <span>{message}</span>
     </div>
   );
