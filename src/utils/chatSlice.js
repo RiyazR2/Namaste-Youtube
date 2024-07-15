0e6c475675d28a9867d336508ebb4de4bbd119b8
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-15 15:37:52 +0530

    Updates on top chat delete

--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -7,8 +7,14 @@ const chatSlice = createSlice({
     messages: [],
   },
   reducers: {
+    // addMessage: (state, action) => {
+    //   state.messages.splice(LIVE_CHAT_COUNT, 1);
+    //   state.messages.push(action.payload);
+    // },
     addMessage: (state, action) => {
-      state.messages.splice(LIVE_CHAT_COUNT, 1);
+      if (state.messages.length >= LIVE_CHAT_COUNT) {
+        state.messages.shift();
+      }
       state.messages.push(action.payload);
     },
   },
