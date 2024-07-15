6cc0f3a6c236898de7ccb875ad236777b145f3a7
RiyazR2 <riyazpathan193.rp@gmail.com>
2024-07-15 18:34:54 +0530

    Updated Chat deletion

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -17,17 +17,17 @@ const LiveChat = () => {
       dispatch(
         addMessage({
           name: generateRandomName(),
-          message: makeRandomMessage(20) + " 🚀",
+          message: makeRandomMessage() + "🚀",
         })
       );
-    }, 2000);
+    }, 1000);
 
     return () => clearInterval(i);
   }, []);
 
   return (
     <>
-      <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+      <div className="w-auto h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
         <div>
           {
             // Disclaimer: Don't use indexes as keys
@@ -39,28 +39,30 @@ const LiveChat = () => {
       </div>
 
       <form
-        className="w-full p-2 ml-2 border border-black"
+        className="w-full p-1 ml-2 border border-black flex"
         onSubmit={(e) => {
           e.preventDefault();
 
           dispatch(
             addMessage({
-              name: "Akshay Saini",
+              name: "Riyaz Pathan",
               message: liveMessage,
             })
           );
+
+          // to clear input after send
           setLiveMessage("");
         }}
       >
         <input
-          className="px-2 w-96"
+          className="px-2 w-[320px] border border-black "
           type="text"
           value={liveMessage}
           onChange={(e) => {
             setLiveMessage(e.target.value);
           }}
         />
-        <button className="px-2 mx-2 bg-green-100">Send</button>
+        <button className="px-2 mx-1 bg-green-100">Send</button>
       </form>
     </>
   );
